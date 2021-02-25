21e61df5956934a4c26864844e064814ecde0e8e
Ikusa <22572595+Ikusa0@users.noreply.github.com>
2021-02-25 14:40:30 -0300

    Terceiro dia da NLW4

--- API/src/controller/UserController.ts
+++ API/src/controller/UserController.ts
@@ -1,12 +1,14 @@
 import {Request, Response} from "express";
-import {getRepository} from "typeorm";
-import {User} from "../models/User";
+import {getCustomRepository} from "typeorm";
+import {UserRepository} from "../repositories/UserRepository";
+import {SurveyRepository} from "../repositories/SurveyRepository";
 
 class UserController {
+
     async create(request: Request, response: Response) {
         const {name, email} = request.body;
 
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UserRepository);
 
         const userAlreadyExists = await usersRepository.findOne({
             email
@@ -24,7 +26,7 @@ class UserController {
 
         await usersRepository.save(user);
 
-        return response.json(user);
+        return response.status(201).json(user);
     }
 }
 
--- API/src/routes.ts
+++ API/src/routes.ts
@@ -1,10 +1,15 @@
 import {Router} from "express";
 import {UserController} from "./controller/UserController";
+import {SurveyController} from "./controller/SurveyController";
 
 const router = Router();
 
 const userController = new UserController();
+const surveyController = new SurveyController();
 
 router.post("/users", userController.create);
+
+router.post("/surveys", surveyController.create);
+router.get("/surveys", surveyController.show);
 
 export {router};
